0d9e4e10729a2b8b37a581ffc0349d88f48ec402
Kava Developers <mehul9921@gmail.com>
2021-09-25 17:12:58 +0530

    Update about.php

--- application/views/about.php
+++ application/views/about.php
@@ -34,28 +34,28 @@
                 <div class="count-box">
                     <i class="icofont-simple-smile"></i>
                     <span data-toggle="counter-up">232</span>
-                    <p><strong>Happy Clients</strong> consequuntur quae qui deca rode</p>
+                    <p><strong>Happy Clients</strong></p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
                 <div class="count-box">
                     <i class="icofont-document-folder"></i>
-                    <span data-toggle="counter-up">521</span>
-                    <p><strong>Projects</strong> adipisci atque cum quia aut numquam delectus</p>
+                    <span data-toggle="counter-up">8555</span>
+                    <p><strong>Export Items</strong></p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
                 <div class="count-box">
                     <i class="icofont-live-support"></i>
-                    <span data-toggle="counter-up">1,463</span>
-                    <p><strong>Hours Of Support</strong> aut commodi quaerat. Aliquam ratione</p>
+                    <span>24/7</span>
+                    <p><strong>Hours Of Support</strong></p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
                 <div class="count-box">
                     <i class="icofont-users-alt-5"></i>
-                    <span data-toggle="counter-up">15</span>
-                    <p><strong>Hard Workers</strong> rerum asperiores dolor molestiae doloribu</p>
+                    <span data-toggle="counter-up">27</span>
+                    <p><strong>Hard Workers</strong></p>
                 </div>
             </div>
         </div>
